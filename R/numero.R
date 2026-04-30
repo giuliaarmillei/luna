a89602f0cc12754aa766d28e297f5cc68e7a7b0c
Giulia Armillei <giuliaarm00@icloud.com>
2026-04-30 15:02:29 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,12 @@
+# funcione if else
+numero <- function(x){
+  if(x>0) {
+    "Questo numero è positivo"
+    }
+  else if(x<0) {
+    "Questo numero è negativo"
+    }
+  else {
+    "Zero non è negativo nè positivo"
+    }
+  }
